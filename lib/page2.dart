8df67df071023a9e7a09be7d97f4758848ededc3
valas <larsien85@gmail.com>
2022-09-16 19:49:42 +0900

    Update page2.dart

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,7 +1,5 @@
-import 'package:event_ios_mobile_app_animation_by_ronasit/common.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-
+import 'common.dart';
 import 'page3.dart';
 
 class Page2 extends StatefulWidget {
@@ -21,17 +19,14 @@ class _Page2State extends State<Page2> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 3), vsync: this);
-    //for text
     headerTextFadeInAnimation = Tween(begin: 0.0, end: 1.0).animate(
         CurvedAnimation(parent: controller, curve: Curves.easeOutCubic));
-
     headerTextSlideUpAnimation = Tween<Offset>(
             begin: const Offset(0, 1), end: const Offset(0, 0))
         .animate(CurvedAnimation(parent: controller, curve: Curves.elasticOut));
     controller.forward();
   }
 
-  List<bool> boolList = [true, true, true];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,8 +39,6 @@ class _Page2State extends State<Page2> with SingleTickerProviderStateMixin {
         ),
       ),
     );
-
-    // return const FlutterLogo();
   }
 
   @override
@@ -55,187 +48,8 @@ class _Page2State extends State<Page2> with SingleTickerProviderStateMixin {
   }
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key, required this.controller});
-  final AnimationController controller;
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: sampleContentList.length,
-        itemBuilder: (context, index) {
-          var item = sampleContentList[index];
-          Animation<double> itemOpacityAnimation = Tween(begin: 0.0, end: 1.0)
-              .animate(CurvedAnimation(
-                  parent: controller,
-                  curve: Interval(0.1 * index, 0.6 + index * 0.1,
-                      curve: Curves.linear)));
-          Animation<Offset> itemSlideAnimation =
-              Tween<Offset>(begin: const Offset(0, 0.3), end: Offset.zero)
-                  .animate(CurvedAnimation(
-                      parent: controller,
-                      curve: Interval(0.1 * index, 0.6 + index * 0.1,
-                          curve: Curves.linear)));
-          return GestureDetector(
-            onTap: () => Navigator.push(context, delayedRoute(index)),
-            // MaterialPageRoute(builder: (contgext) => Page2(index))),
-            child: Item(item: item, index: index),
-          );
-        },
-      ),
-    );
-  }
-
-  Route delayedRoute(int index) {
-    return PageRouteBuilder(
-      pageBuilder: (_, __, ___) => Page3(index),
-      transitionDuration: const Duration(milliseconds: 1200),
-    );
-  }
-}
-
-class Item extends StatelessWidget {
-  const Item({super.key, required this.item, required this.index});
-
-  final Map<String, String> item;
-  final int index;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      // crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Text(item["dayOfWeek"]!, style: const TextStyle(fontSize: 12)),
-              const SizedBox(height: 5),
-              Text(item["day"]!,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      color: Color(primaryRedColor),
-                      fontWeight: FontWeight.bold))
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Stack(
-            children: [
-              Hero(
-                tag: "content$index",
-                child: CardBody(item: item),
-              ),
-              Hero(
-                tag: "test$index",
-                child: CardImage(item: item),
-              ),
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class CardImage extends StatelessWidget {
-  const CardImage({
-    super.key,
-    required this.item,
-  });
-
-  final Map<String, String> item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 12, top: 12),
-      child: Container(
-        width: 50,
-        height: 80,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image:
-                    //  Image.asset(item["image"]!)
-                    AssetImage(
-                  item["image"]!,
-                )),
-            borderRadius: const BorderRadius.all(Radius.circular(20))),
-      ),
-    );
-  }
-}
-
-class CardBody extends StatelessWidget {
-  const CardBody({
-    super.key,
-    required this.item,
-  });
-
-  final Map<String, String> item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      elevation: 15,
-      child: Row(
-        // mainAxisAlignment: MainAxisAlignment.end,
-        // mainAxisSize: MainAxisSize.max,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: 50,
-              height: 80,
-            ),
-          ),
-          SizedBox(
-            width: 200,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(item["title"]!,
-                      maxLines: 2,
-                      overflow: TextOverflow.clip,
-                      style: const TextStyle(fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 5),
-                  Text(item["location"]!)
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Header extends StatelessWidget {
-  const Header({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text("Last results",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-    );
-  }
-}
-
 class CustomCoffeeAppBar extends StatefulWidget with PreferredSizeWidget {
-// class CustomAppBar extends AppBar {
-  // ignore: use_key_in_widget_constructors
-  CustomCoffeeAppBar(this.controller);
+  CustomCoffeeAppBar(this.controller, {super.key});
   final AnimationController controller;
   @override
   State<CustomCoffeeAppBar> createState() => _CustomCoffeeAppBarState();
@@ -246,24 +60,10 @@ class CustomCoffeeAppBar extends StatefulWidget with PreferredSizeWidget {
 
 class _CustomCoffeeAppBarState extends State<CustomCoffeeAppBar> {
   late AnimationController controller;
-  late Animation<Offset> cupSlideUpAnimation;
-  late Animation<double> cupRotateAnimation;
-  late Animation<double> cupFadeInAnimation;
-  late Animation<int> textStepAnimation;
-
   bool isLoading = true;
   @override
   void initState() {
     controller = widget.controller;
-    //for cup
-    cupRotateAnimation = Tween(begin: 0.0, end: 0.07).animate(
-        CurvedAnimation(parent: controller, curve: const ShakeCurve(count: 3)));
-    cupSlideUpAnimation = Tween(
-            begin: const Offset(0, 1), end: const Offset(0, 0))
-        .animate(CurvedAnimation(parent: controller, curve: Curves.elasticOut));
-
-    cupFadeInAnimation = Tween(begin: 0.0, end: 1.0).animate(
-        CurvedAnimation(parent: controller, curve: Curves.easeOutCubic));
 
     super.initState();
     Future.delayed(const Duration(milliseconds: 50)).then((value) {
@@ -286,7 +86,7 @@ class _CustomCoffeeAppBarState extends State<CustomCoffeeAppBar> {
         duration: const Duration(seconds: 3),
         curve: Curves.ease,
         child: Transform.rotate(
-          angle: isLoading ? 0.0 : 1.5,
+          angle: isLoading ? 0.0 : 0.5,
           child: SizedBox(
             height: 70,
             //https://www.pngwing.com/en/free-png-zqryl
@@ -343,22 +143,8 @@ class _MenuBarState extends State<MenuBar> {
                     isMenu1Selected = true;
                     setState(() {});
                   },
-                  child: Container(
-                      // width: 60,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(17),
-                          color: isMenu1Selected
-                              ? const Color.fromARGB(255, 2, 58, 105)
-                              : Colors.white),
-                      child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 25),
-                          child: Text('Events',
-                              style: TextStyle(
-                                  color: isMenu1Selected
-                                      ? Colors.white
-                                      : Colors.grey))))),
+                  child: AppBarMenuContainer(
+                      isMenuSelected: isMenu1Selected, menuName: "Events")),
               const SizedBox(
                 width: 10,
               ),
@@ -367,24 +153,36 @@ class _MenuBarState extends State<MenuBar> {
                     isMenu1Selected = false;
                     setState(() {});
                   },
-                  child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(17),
-                          color: isMenu1Selected
-                              ? Colors.white
-                              : const Color.fromARGB(255, 2, 58, 105)),
-                      child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Text('Organizers',
-                              style: TextStyle(
-                                  color: isMenu1Selected
-                                      ? Colors.grey
-                                      : Colors.white))))),
+                  child: AppBarMenuContainer(
+                      isMenuSelected: !isMenu1Selected, menuName: "Organizers"))
             ]),
           ),
         ),
       ),
     );
+  }
+}
+
+class AppBarMenuContainer extends StatelessWidget {
+  final bool isMenuSelected;
+  final String menuName;
+  const AppBarMenuContainer(
+      {super.key, required this.isMenuSelected, required this.menuName});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // width: 60,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(17),
+            color: isMenuSelected
+                ? const Color.fromARGB(255, 2, 58, 105)
+                : Colors.white),
+        child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Text(menuName,
+                style: TextStyle(
+                    color: isMenuSelected ? Colors.white : Colors.grey))));
   }
 }
 
@@ -431,8 +229,6 @@ class AppBarBottomWhiteBackground extends StatelessWidget {
         child: Divider(
           thickness: 60,
           color: Colors.white,
-          // decoration: const BoxDecoration(color: Colors.blue),
-          // child: const SizedBox(height: 60))
         ));
   }
 }
@@ -457,13 +253,165 @@ class AppBarBackground extends StatelessWidget {
   }
 }
 
-class ShakeCurve extends Curve {
-  final double count;
-  const ShakeCurve({this.count = 1});
+class Header extends StatelessWidget {
+  const Header({
+    super.key,
+  });
 
   @override
-  double transformInternal(double t) {
-    var val = sin(count * 2 * pi * t + 0.5) * 0.5 + 0.7;
-    return val;
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text("Last results",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key, required this.controller});
+  final AnimationController controller;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: sampleContentList.length,
+        itemBuilder: (context, index) {
+          var item = sampleContentList[index];
+
+          return GestureDetector(
+            onTap: () => Navigator.push(context, _delayedRoute(index)),
+            child: ListItem(item: item, index: index),
+          );
+        },
+      ),
+    );
+  }
+
+  Route _delayedRoute(int index) {
+    return PageRouteBuilder(
+      pageBuilder: (_, __, ___) => Page3(index),
+      transitionDuration: const Duration(milliseconds: 1200),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key, required this.item, required this.index});
+
+  final Map<String, String> item;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(item["dayOfWeek"]!, style: const TextStyle(fontSize: 12)),
+              const SizedBox(height: 5),
+              Text(item["day"]!,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      color: Color(primaryRedColor),
+                      fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              Hero(
+                tag: "content$index",
+                child: CardBody(item: item),
+              ),
+              Hero(
+                tag: "image$index",
+                child: CardImage(item: item),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CardImage extends StatelessWidget {
+  const CardImage({
+    super.key,
+    required this.item,
+  });
+
+  final Map<String, String> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 12, top: 12),
+      child: Container(
+        width: 50,
+        height: 80,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image:
+                    //  Image.asset(item["image"]!)
+                    AssetImage(
+                  item["image"]!,
+                )),
+            borderRadius: const BorderRadius.all(Radius.circular(20))),
+      ),
+    );
+  }
+}
+
+class CardBody extends StatelessWidget {
+  const CardBody({
+    super.key,
+    required this.item,
+  });
+
+  final Map<String, String> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      elevation: 15,
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 50,
+              height: 80,
+            ),
+          ),
+          SizedBox(
+            width: 200,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(item["title"]!,
+                      maxLines: 2,
+                      overflow: TextOverflow.clip,
+                      style: const TextStyle(fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 5),
+                  Text(item["location"]!)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
