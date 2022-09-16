d631d03edca864764bf821b91797d7ff3dc14598
valas <larsien85@gmail.com>
2022-09-16 19:48:53 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,21 @@ import 'package:flutter/material.dart';
 import 'page1.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        initialRoute: "/",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Page1());
+  }
 }
